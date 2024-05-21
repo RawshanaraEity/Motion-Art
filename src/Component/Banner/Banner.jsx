0fb4d7a24c9a5ff8005c60b7988cfe7e984be831
Rawshanara Eity <rawshanara.eity@gmail.com>
2024-05-21 15:50:55 +0600

    added gradient color on text

---
+++ src/Component/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+const Banner = () => {
+  return (
+    <div>
+      <div className="pt-10">
+        <img src="/public/image/MotionArtEffect-logo.png" alt="" />
+      </div>
+      <div className="w-[640px] mx-auto">
+        <h1 className="text-[55px] font-medium leading-[65px]">
+          Attract Your Visitors Attention With <br /> Colorful <br />
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#F87516] via-[#5E11FF] to-[#5E11FF]"> Motion Art Effect</span>
+        </h1>
+        <p className="text-[#EEE5FFBD] text-[18px] font-light leading-7 py-8">
+          Unleash the power of creativity with Motion Art for Elementor - your
+          ultimate solution for seamlessly integrating captivating animations
+          into your website.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../../Component/Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div className="bg-[#0D051F] text-[#EEE5FF]">
+           <Banner/>
+        </div>
+    );
+};
+
+export default Home;
