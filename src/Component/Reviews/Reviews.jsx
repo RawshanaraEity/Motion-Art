0cdf5a5f80f0a8922c8ca1ba9bd10cc7de681820
Rawshanara Eity <rawshanara.eity@gmail.com>
2024-05-21 21:09:25 +0600

    added review section

---
+++ src/Component/Reviews/Reviews.jsx
@@ -0,0 +1,45 @@
+
+
+const Reviews = () => {
+    return (
+        <div className="pt-10 pb-20">
+            <h2 className="text-center text-[22px] font-normal">
+            Trusted by thousands of users around the world
+            </h2>
+           <div className="flex justify-between">
+            {/* item 1 */}
+           <div className="flex items-center gap-3 px-5 pt-[50px]">
+                <div>
+                    <img src="/public/image/motionarteffect-img2.png" alt="" />
+                </div>
+                <div>
+                    <img src="/public/image/motionarteffect-img4.png" alt="" />
+                    <p className="pt-5">4.5 Score, 9 Reviews</p>
+                </div>
+            </div>
+            {/*item 2 */}
+           <div className="flex items-center gap-3 px-5 pt-[50px]">
+                <div>
+                    <img src="/public/image/motionarteffect-img1.png" alt="" />
+                </div>
+                <div>
+                    <img src="/public/image/motionarteffect-img4.png" alt="" />
+                    <p className="pt-5">4.5 Score, 9 Reviews</p>
+                </div>
+            </div>
+            {/* item 3 */}
+           <div className="flex items-center gap-3 px-5 pt-[50px]">
+                <div>
+                    <img src="/public/image/motionarteffect-img3.png" alt="" />
+                </div>
+                <div>
+                    <img src="/public/image/motionarteffect-img4.png" alt="" />
+                    <p className="pt-5">4.5 Score, 9 Reviews</p>
+                </div>
+            </div>
+           </div>
+        </div>
+    );
+};
+
+export default Reviews;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Component/Banner/Banner";
+import Reviews from "../../Component/Reviews/Reviews";
 
 
 const Home = () => {
     return (
         <div className="bg-[#0D051F] text-[#EEE5FF]">
            <Banner/>
+           <Reviews/>
         </div>
     );
 };
